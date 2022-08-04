2c838bbd5d78927b85cf72cf3b8d74608b120e6b
LeonardoSGSG <56612664+LeonardoSGSG@users.noreply.github.com>
2022-08-04 16:31:28 -0500

    Update tamanho.cs

--- Assets/Scripts/tamanho.cs
+++ Assets/Scripts/tamanho.cs
@@ -8,12 +8,15 @@ public class tamanho : MonoBehaviour
     Vector3 posInicial;
     Quaternion rotationI;
     public GameObject foco;
+    public bool agarrado;
+    public Color colorI;
     // Start is called before the first frame update
     void Start()
     {
         escInicial = transform.localScale;
         posInicial = transform.position;
         rotationI = transform.rotation;
+        colorI = transform.GetComponent<Renderer>().material.color;
     }
 
     // Update is called once per frame
@@ -23,10 +26,18 @@ public class tamanho : MonoBehaviour
         {
             transform.GetComponent<Rigidbody>().useGravity = true;
             transform.GetComponent<Rigidbody>().isKinematic = false;
+            if (agarrado)
+            {
+                transform.GetComponent<Renderer>().material.color = Color.green;
+            }
         }
+        transform.GetComponent<Material>().color = colorI;
+
+
     }
     public void achicar()
     {
+        agarrado = true;
         if(transform.localScale==escInicial)
         {
             transform.localScale = new Vector3(transform.localScale.x * 0.3f, transform.localScale.y * 0.3f, transform.localScale.z * 0.3f);
@@ -36,6 +47,7 @@ public class tamanho : MonoBehaviour
     }
     public void agrandar()
     {
+        agarrado = false;
         //transform.localScale = escInicial;
         //transform.position = posInicial;
         //transform.rotation = rotationI;
