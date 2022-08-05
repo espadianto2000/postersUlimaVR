739cee7c4dce059c5d83c1ad54d8781eaa2b752e
LeonardoSGSG <56612664+LeonardoSGSG@users.noreply.github.com>
2022-08-05 09:08:57 -0500

    Update TwoHand.cs

--- Assets/Scripts/TwoHand.cs
+++ Assets/Scripts/TwoHand.cs
@@ -16,9 +16,10 @@ public class TwoHand : XRGrabInteractable
     {
         
     }
- /*   public override bool IsSelectableBy(XRBaseInteractor interactor)
+    public override bool IsSelectableBy(XRBaseInteractor interactor)
     {
         bool isalreadygrabbed = selectingInteractor && !interactor.Equals(selectingInteractor);
-        return base.IsSelectableBy(interactor) && isalreadygrabbed;
-    }*/
+        return base.IsSelectableBy(interactor) && !isalreadygrabbed;
+    }
 }
+ 
